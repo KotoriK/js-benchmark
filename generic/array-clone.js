b2370f93732ccfc63bde068579de9f1c7aba4e5f
KotoriK <52659125+KotoriK@users.noreply.github.com>
2024-01-09 14:47:52 +0800

    fix: typo

--- generic/array-clone.js
+++ generic/array-clone.js
@@ -79,7 +79,7 @@ function run(testCase) {
     console.groupEnd();
 }
 
-for (let i = 2; i < 32; i += 6) {
+for (let i = 2; i < 32; i += 3) {
     const testCase = prepareCase(2 ** i);
     run(testCase)
 }
